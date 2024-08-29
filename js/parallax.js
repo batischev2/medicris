d0525b2d55de3fbfab8125252327d3aeba357c45
Victor Batischev <vitek.batishev97@gmail.com>
2024-08-29 13:37:16 +0300

    styles

--- js/parallax.js
+++ js/parallax.js
@@ -3,14 +3,14 @@ const page2 = document.getElementById('page-2')
 const gradient = document.getElementById('gradient')
 
 window.addEventListener('scroll', () => {
-  if (Math.round(scrollY) > 617) {
+  if (Math.round(scrollY) > 550) {
     page2.classList.add('page-2-active')
     page2.style.cssText = 'opacity: 100'
   }
-  if (Math.round(scrollY) > 3000) {
+  if (Math.round(scrollY) > 4000) {
     page2.style.cssText = `opacity: 0;`
   }
-  if (Math.round(scrollY) < 616) {
+  if (Math.round(scrollY) < 550) {
     page2.classList.remove('page-2-active')
   }
 })
